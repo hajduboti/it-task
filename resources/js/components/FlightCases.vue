39868a7f920f82e170181624efce9f036fa53806
unknown <boto0018@stud.kea.dk>
2019-10-26 12:06:40 +0200

    first commit

---
+++ resources/js/components/FlightCases.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Flight cases</div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
